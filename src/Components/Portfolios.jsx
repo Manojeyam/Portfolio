fd34f55d3fb22864c2cf914092eb21e29acbf1f0
Manojeyam <manojeyam1234@gmail.com>
2024-05-05 21:08:39 +0530

    cfr

--- src/Components/Portfolios.jsx
+++ src/Components/Portfolios.jsx
@@ -898,7 +898,7 @@ College.
                                 , justifyContent: "space-evenly"
                             }}>
                                 <Box>
-                                    <Box component="a" href='https://manojeyam26.artstation.com/' target='blank' sx={{ textDecoration: "none", color: "white", cursor: "pointer" }}><FaArtstation style={{ fontSize: "50px !important" }} /></Box>
+                                    <Box component="a" href='https://manojeyam26.artstation.com/' target='blank' sx={{ textDecoration: "none", color: "white", cursor: "pointer" }}><FaArtstation style={{ fontSize: "50px !important",boxShadow:'none !important' }} /></Box>
                                 </Box>
                                 <Box>
                                     <Box component="a" sx={{ textDecoration: "none", color: "white", cursor: "pointer" }} target='blank' href='linkedin.com/in/manojeyam26'><CiLinkedin style={{ fontSize: "45px", borderRadius: "100%", }} /></Box>
